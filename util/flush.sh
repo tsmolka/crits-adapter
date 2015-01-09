65db8ad0dd3b6596752f0560d74759a0fd190889
Trey Darley <trey@kingfisherops.com>
2015-01-09 14:05:11 +0100

    add utility script for wiping data from edge / crits

---
+++ util/flush.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+service httpd stop
+service edgy_critsd stop
+# flush edge mongodb
+mongo inbox --eval "db.stix.remove({})"
+mongo inbox --eval "db.uploads.remove({})"
+mongo inbox --eval "db.adapters.crits.remove({})"
+mongo inbox --eval "db.stats_indicators.remove({})"
+mongo inbox --eval "db.indicators_by_date.remove({})"
+mongo inbox --eval "db.uploads.remove({})"
+mongo inbox --eval "db.peer_push.remove({})"
+mongo inbox --eval "db.peer_sessions.remove({})"
+# flush crits mongodb
+mongo crits --eval "db.ips.remove({})"
+mongo crits --eval "db.domains.remove({})"
+mongo crits --eval "db.sample.remove({})"
+mongo crits --eval "db.email.remove({})"
+mongo crits --eval "db.indicators.remove({})"
+# delete old logs
+rm -f /opt/soltra/edge/repository/adapters/crits/edgy_crits*.log*
+
+service httpd start
+service edgy_critsd start
