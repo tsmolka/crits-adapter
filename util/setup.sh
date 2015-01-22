b83930f8632a601c56a4fca3badb7541f1a644a5
Trey Darley <trey@kingfisherops.com>
2015-01-22 13:54:12 +0100

    update setup.sh to install adapter python depends from yum

--- util/setup.sh
+++ util/setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-yum install -y libyaml-devel
-/opt/soltra/edge/bin/pip2.7 install Logbook==0.8.1
-/opt/soltra/edge/bin/pip2.7 install PyYAML==3.11
-/opt/soltra/edge/bin/pip2.7 install docopt==0.6.2
-/opt/soltra/edge/bin/pip2.7 install ipaddr==2.1.11
-/opt/soltra/edge/bin/pip2.7 install requests==2.5.1
+#
+# install soltra edge crits adapter
+#
+
+yum install -y soltra-edge-python-pyyaml \
+    soltra-edge-python-requests \
+    soltra-edge-python-docopt \
+    soltra-edge-python-logbook
 chown -R repo.repo /opt/soltra/edge/repository/adapters/crits/
 chmod -R ug+rw,o-rwx /opt/soltra/edge/repository/adapters/crits/
 find /opt/soltra/edge/repository/adapters/crits/ -type d -exec chmod ug+x {} \;
