692122c9f3f588db9cbd810bbda279c61a6a8689
Trey Darley <trey@kingfisherops.com>
2015-01-23 11:45:03 +0100

    prompt the user to edit config.yaml and start the service

--- util/setup.sh
+++ util/setup.sh
@@ -12,7 +12,8 @@ chown -R repo.repo /opt/soltra/edge/repository/adapters/crits/
 chmod -R ug+rw,o-rwx /opt/soltra/edge/repository/adapters/crits/
 find /opt/soltra/edge/repository/adapters/crits/ -type d -exec chmod ug+x {} \;
 chmod a+x /opt/soltra/edge/repository/adapters/crits/*.py
-chmod a+x /opt/soltra/edge/repository/adapters/crits/util/*.{py,sh}
+chmod a+x /opt/soltra/edge/repository/adapters/crits/util/*
 ln -s /opt/soltra/edge/repository/adapters/crits/init.d-edgy_critsd /etc/init.d/edgy_critsd
 chkconfig --level 345 edgy_critsd on
 echo '/opt/soltra/edge/repository/lib' >> /opt/soltra/edge/lib/python2.7/site-packages/repository.pth
+echo 'edgy_critsd successfully installed. Edit config.yaml and then start the service (`service edgy_critsd start`).'
