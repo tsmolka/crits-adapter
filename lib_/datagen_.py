7f2e71fcde7e9648929e3ccb74ae24be847a25af
Trey Darley <trey@kingfisherops.com>
2015-01-28 12:00:12 +0100

    refactor / tidy up (pep8)

--- lib_/datagen_.py
+++ lib_/datagen_.py
@@ -28,36 +28,37 @@ def load_mail_header_bits(config):
 
 
 def get_random_spam_msg(config):
-    header_map = {'Subject': 'subject', 'To': 'to', 'Cc':
-              'cc', 'Bcc': 'bcc', 'From': 'from_',
-              'Sender': 'sender', 'Date': 'date',
-              'Message-ID': 'message_id', 'Reply-To':
-              'reply_to', 'In-Reply-To': 'in_reply_to',
-              'Content-Type': 'content_type', 'Errors-To':
-              'errors_to', 'Precedence': 'precedence',
-              'Boundary': 'boundary', 'MIME-Version':
-              'mime_version', 'X-Mailer': 'x_mailer',
-              'User-Agent': 'user_agent',
-              'X-Originating-IP': 'x_originating_ip',
-              'X-Priority': 'x_priority'}
+    header_map = {'Subject': 'subject', 'To': 'to',
+                  'Cc': 'cc', 'Bcc': 'bcc', 'From': 'from_',
+                  'Sender': 'sender', 'Date': 'date',
+                  'Message-ID': 'message_id', 'Reply-To': 'reply_to',
+                  'In-Reply-To': 'in_reply_to', 'Content-Type': 'content_type',
+                  'Errors-To': 'errors_to', 'Precedence': 'precedence',
+                  'Boundary': 'boundary', 'MIME-Version': 'mime_version',
+                  'X-Mailer': 'x_mailer', 'User-Agent': 'user_agent',
+                  'X-Originating-IP': 'x_originating_ip',
+                  'X-Priority': 'x_priority'}
     msg = dict()
     for key_ in header_map.keys():
         if key_ in config['datagen']['email_headers'].keys():
             msg[key_] = random.choice(config['datagen']['email_headers'][key_])
     return(msg)
-    
+
 
 def get_email_payload(msg):
     val = None
     # msg has no body
-    if not hasattr(msg, 'get_content_maintype'): return(val)
+    if not hasattr(msg, 'get_content_maintype'):
+        return(val)
     type_ = msg.get_content_maintype()
     if type_ == 'multipart':
         # msg has no body
-        if not hasattr(msg, 'get_payload'): return(val)
+        if not hasattr(msg, 'get_payload'):
+            return(val)
         for part in msg.get_payload():
             # probably this is a malformed msg
-            if not hasattr(part, 'get_content_type_'): continue
+            if not hasattr(part, 'get_content_type_'):
+                continue
             if part.get_content_type_() == 'text':
                 val = part.get_payload()
             elif type_ == 'text':
@@ -77,12 +78,14 @@ def generate_random_hashes():
     hashes['sha512'] = sha512(val).hexdigest()
     # hashes['ssdeep'] = ssdeep.hash(val)
     return(hashes)
-    
+
 
 def generate_random_domain(config):
-    '''generate a random domain name by pairing a random uuid with a random tld'''
+    '''generate a random domain name by pairing a random uuid with a
+    random tld'''
     domain = str(uuid.uuid4())
-    tld = config['datagen']['tlds'][random.randint(0, len(config['datagen']['tlds']) - 1)]
+    tld_index = random.randint(0, len(config['datagen']['tlds']) - 1)
+    tld = config['datagen']['tlds'][tld_index]
     domain += '.%s' % tld
     return(domain)
 
