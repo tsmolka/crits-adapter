7d23c7b92671f1145c40cf1857c7490323d11f51
Trey Darley <trey@kingfisherops.com>
2015-02-25 04:19:50 -0500

    add comments

--- util/flush.sh
+++ util/flush.sh
@@ -2,11 +2,12 @@
 
 service httpd stop
 service edgy_critsd stop
-flush edge mongodb
+# flush edge mongodb
 EDGE_COLLECTIONS='stix uploads adapters.crits stats_indicators cache indicators_by_date peer_log peer_sessions'
 for EDGE_COLLECTION in ${EDGE_COLLECTIONS}; do
     mongo inbox --eval "db.${EDGE_COLLECTION}.remove({})"
 done
+# flush crits mongodb
 CRITS_COLLECTIONS='ips domains sample email indicators events'
 for CRITS_COLLECTION in ${CRITS_COLLECTIONS}; do
     mongo crits --eval "db.${CRITS_COLLECTION}.remove({})"
